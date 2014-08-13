8d3642ae8ccca8f952f8a1e864bda30a63ba1eaf
ka <ka.kaosf@gmail.com>
2014-08-14 00:41:33 +0900

    Clean up (Remove all completely)

--- config/initializers/session_store.rb
+++
@@ -1,8 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-EnvVarHeroku::Application.config.session_store :cookie_store, key: '_env-var-heroku_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# EnvVarHeroku::Application.config.session_store :active_record_store
